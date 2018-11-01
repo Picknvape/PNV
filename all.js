b87ab13aaa6a8f483956d4f48ba184fd99237fd0
Dmitry Katorzhnov <katorzhnov@Dmitrys-MBP.lan>
2018-11-01 17:06:44 +0300

    Анимацию обновил

--- all.js
+++ all.js
@@ -48,53 +48,54 @@ function timer() {
 
 // Появлялка-убиралка бонусов по ховеру на описание
 var randomItemsContainerBaseClass = document.getElementById('bonus-rotator').className;
-function RandomUISetup(firstInit=false) {
-	var randomItemsContainer = document.getElementById('bonus-rotator');
-	var randomItemsContainerChildren = randomItemsContainer.getElementsByClassName('bonus-rotator-item');
-	var bonusesHighlightenedDescriptions = document.getElementsByClassName('bonuses-description-highlighted');
-	if (firstInit) {
-		firstInit = false;
-		for (let i = 0; i < bonusesHighlightenedDescriptions.length; i++) {
-			let oldClasses = randomItemsContainer.className;
-			let newClass = oldClasses + ' highlight ' + bonusesHighlightenedDescriptions[i].classList[1];
-			bonusesHighlightenedDescriptions[i].addEventListener('click', function() {
-				randomItemsContainer.className = newClass;
-			});	
-		}
-	}
-	for (let i = randomItemsContainerChildren.length; i >= 0; i--) {
-		randomItemsContainer.appendChild(randomItemsContainerChildren[Math.random() * i | 0]);
-	}
+
+function RandomUISetup(firstInit = false) {
+  var randomItemsContainer = document.getElementById('bonus-rotator');
+  var randomItemsContainerChildren = randomItemsContainer.getElementsByClassName('bonus-rotator-item');
+  var bonusesHighlightenedDescriptions = document.getElementsByClassName('bonuses-description-highlighted');
+  if (firstInit) {
+    firstInit = false;
+    for (let i = 0; i < bonusesHighlightenedDescriptions.length; i++) {
+      let oldClasses = randomItemsContainer.className;
+      let newClass = oldClasses + ' highlight ' + bonusesHighlightenedDescriptions[i].classList[1];
+      bonusesHighlightenedDescriptions[i].addEventListener('click', function() {
+        randomItemsContainer.className = newClass;
+      });
+    }
+  }
+  for (let i = randomItemsContainerChildren.length; i >= 0; i--) {
+    randomItemsContainer.appendChild(randomItemsContainerChildren[Math.random() * i | 0]);
+  }
 }
 RandomUISetup(true);
 
 document.getElementById('get-bonus-button').addEventListener('click', DoPseudoRandomAnimationCycle);
 var maxStep = document.getElementById('bonus-rotator').children.length; //changeable, use as public editable variable to adjust amount of steps in animation according to baked elements count
 var randomStep = maxStep;
-var frameTime = 750;
+var frameTime = 400;
 
 function DoPseudoRandomAnimationCycle() {
-	let randomItemsContainer = document.getElementById('bonus-rotator');
-	randomItemsContainer.className = randomItemsContainerBaseClass;
-	if (randomStep == 0) {
-		randomItemsContainer.className = randomItemsContainerBaseClass;
-		RandomUISetup();
-		randomStep = maxStep;	
-	}
+  let randomItemsContainer = document.getElementById('bonus-rotator');
+  randomItemsContainer.className = randomItemsContainerBaseClass;
+  if (randomStep == 0) {
+    randomItemsContainer.className = randomItemsContainerBaseClass;
+    RandomUISetup();
+    randomStep = maxStep;
+  }
   var animationStep = function() {
     if (--randomStep > 0) {
-      if (randomStep != maxStep-1) {
-        document.getElementById('random-item-' + (randomStep+1)).classList.remove('checked');
+      if (randomStep != maxStep - 1) {
+        document.getElementById('random-item-' + (randomStep + 1)).classList.remove('checked');
       }
       document.getElementById('random-item-' + randomStep).classList.add('checked');
-	  let powMod = 2;
-	  let intMod = maxStep;
-	  var newFrameTime = frameTime*((-(Math.pow((randomStep-intMod),powMod) / Math.pow(intMod,powMod)))+0.95);
-	  console.log("step"+randomStep+" NFT:"+newFrameTime);
+      let powMod = 2;
+      var newFrameTime = ( -Math.pow((randomStep - maxStep / 2), powMod) / Math.pow(maxStep / 2, powMod) + 1) * frameTime;
+      // Math.round((Math.pow((i - iterations / 2), multiplier) / Math.pow(iterations / 2, multiplier) + .25) * frameDurarionMs);
+      console.log("step" + randomStep + " NFT:" + newFrameTime + " funcResult: " + ( -Math.pow((randomStep - maxStep / 2), powMod) / Math.pow(maxStep / 2, powMod) + 1));
       window.setTimeout(animationStep, newFrameTime);
-	  if (randomStep == Math.floor(maxStep / 4)) {
-		randomItemsContainer.classList.toggle('animation-finished');
-	  }
+      if (randomStep == Math.floor(maxStep / 6)) {
+        randomItemsContainer.classList.toggle('animation-finished');
+      }
     } else {
       document.getElementById('random-item-1').classList.remove('checked');
     }
