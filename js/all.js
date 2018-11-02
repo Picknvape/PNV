5f5138767e5b7cc5e94c0480eec85f332cefd29c
Artemiy <rikiworo@gmail.com>
2018-11-02 10:52:44 +0300

    CompileOrderData++

--- js/all.js
+++ js/all.js
@@ -1,15 +1,20 @@
+InitialRadioReader();
 function InitialRadioReader() {
 	CompileOrderData();
 	reflectChoiceOnMap();
-}
+}	
 function CompileOrderData() {
   var orderData = {};
+  orderData.hasAllInfo = true;
   orderData.orderSize = GetChosenBoxSize();
   orderData.tastePreferences = GetPreferences(true);
   orderData.shippingInfo = GetCountryData();
   orderData.clientName = document.getElementById('name-input').value;
+  if (orderData.clientName.length<1) {orderData.hasAllInfo = false;}
   orderData.clientPhone = document.getElementById('phone-input').value;
+  if (orderData.clientPhone.length<1) {orderData.hasAllInfo = false;}
   orderData.clientEmail = document.getElementById('email-input').value;
+  if (orderData.clientEmail.length<1) {orderData.hasAllInfo = false;}
+  if (!orderData.hasAllInfo) {console.log('user info incomplete');}
   return orderData;
 }
-InitialRadioReader();
